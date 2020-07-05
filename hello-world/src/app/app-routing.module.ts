52de7bba7270613aae2119aeb545e4fa255eb953
krwiopijcaSTB <nikodemnokielski@gmail.com>
2020-07-05 15:15:46 +0200

    graphics problems

--- hello-world/src/app/app-routing.module.ts
+++ hello-world/src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {DashboardListComponent} from "./dashboard-list/dashboard-list.component";
+import {LoginListComponent} from "./login-list/login-list.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'dashboard', component: DashboardListComponent},
+  {path: 'login', component: LoginListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [DashboardListComponent, LoginListComponent]
